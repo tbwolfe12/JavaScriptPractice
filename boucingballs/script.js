d32d88b580cb1238b2d546ea1680f6c454c0a80a
Taylor Wolfe <taylorbwolfe10@gmail.com>
2023-09-18 17:34:41 -0400

    made js changes

--- boucingballs/script.js
+++ boucingballs/script.js
@@ -1,12 +1,15 @@
+// a = c, b = tx, c = ty
+
+
 console.log('Hi There!');
 
 let canvas = 
 document.getElementById('canvas');
-let x = canvas.getContext('2d');
-let y = window.innerWidth;
-let z = window.innerHeight;
-canvas.width = y;
-canvas.height = z;
+let a = canvas.getContext('2d');
+let b = window.innerWidth;
+let c = window.innerHeight;
+canvas.width = b;
+canvas.height = c;
 
 let mousex = 0;
 let mousey = 0;
@@ -15,4 +18,44 @@ addEventListener('mousemove', function() {
     mousex = event.clientX;
     mousey = event.clientY;
 });
+
+let g = 0.99;
+canvas.strokeWidth = 5;
+
+function color() {
+     
+        "rgba(" +
+        Math.round(Math.random() * 250) +
+        ',' + 
+        Math.round(Math.random() * 250) +
+        ',' +
+        Math.ceil(Math.random() * 10) / 10
+        +
+        ')';
+    return
+}
+
+function Ball() {
+    this.color = randomColor();
+    this.radius = Math.random() * 20 + 14;
+    this.startRadius = this.radius;
+    this.x = Math.random() * (b - this.radius * 2) + this.radius;
+    this.y = Math.random() * (c - this.radius);
+    this.dy = Math.random() * 2;
+    this.dx = Math.round((Math.random() - 0.5) * 10);
+    this.vel = Math.random() / 5;
+    this.update = function() {
+        a.beginPath();
+        a.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        a.fillStyle = this.color;
+        a.fill();
+    }
+}
+
+let bal = [];
+for (i = 0; i < 50; i++) {
+    bal.push(new Ball());
+}
+
+
 
